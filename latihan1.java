69780eaddc7e6f2ba78bdff8559c24fafed03180
RaraBisri <rarabisri@gmail.com>
2022-11-04 00:13:49 +0800

    Create latihan1.java

---
+++ latihan1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul_3;
+
+/**
+ *
+ * @author USER
+ */
+public class latihan1 {
+    public static void main(String[] args) {
+        System.out.println("Rara Bisri\n" + "2101092017\n" + "Manajemen Informatika\n" + "Teknologi Informasi\n" + "JL. Puti Bungsu, Khatib Sulaiman");
+        System.out.println("Nama\t: Rara\n" + "NoBp\t: 2101092017\n" + "Prodi\t: Manajemen Informatika\n" + "Alamat\t: JL. Puti Bungsu, Khatib Sulaiman\n");
+    }
+}
